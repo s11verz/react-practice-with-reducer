49bc7393ced5810cc5bd449ecaa943c5c7cfc49b
s11verz <72031018+s11verz@users.noreply.github.com>
2023-03-16 16:18:13 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import React, { useContext } from "react";
-
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import MainHeader from "./components/MainHeader/MainHeader";
-import AuthContext from "./store/auth-context";
+import React from 'react';
+import BackwardCounter from './components/BackwardCounter';
+import ForwardCounter from './components/ForwardCounter';
 
 function App() {
-  const ctx = useContext(AuthContext);
   return (
     <React.Fragment>
-      <MainHeader />
-      <main>
-        {!ctx.isLoggedIn && <Login />}
-        {ctx.isLoggedIn && <Home />}
-      </main>
+      <ForwardCounter />
+      <BackwardCounter />
     </React.Fragment>
   );
 }
