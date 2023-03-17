0ad94a968544d762d57e0506d5ff2a22c7c947f2
s11verz <72031018+s11verz@users.noreply.github.com>
2023-03-17 10:44:09 +0900

    사용자 정의 훅 사용 예시

--- src/App.js
+++ src/App.js
@@ -2,42 +2,32 @@ import React, { useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
+import UseHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-practice-1f8e8-default-rtdb.firebaseio.com/task.json"
-      );
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
-  };
+  const { isLoading, error, sendRequest: fetchTasks } = UseHttp();
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({
+          id: taskKey,
+          text: tasksObj[taskKey].text,
+        });
+      }
+      setTasks(loadedTasks);
+    };
+    fetchTasks(
+      {
+        url: "https://react-practice-1f8e8-default-rtdb.firebaseio.com/task.json",
+      },
+      transformTasks
+    );
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
